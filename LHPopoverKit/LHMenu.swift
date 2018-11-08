6cca1d535fa41c62ed8fc36810cf6627cb1add2b
Li-Heng Hsu <yesleon@me.com>
2018-11-09 04:44:59 +0800

    Remove Theme Add Declaritive

--- LHPopoverKit/LHMenu.swift
+++ LHPopoverKit/LHMenu.swift
@@ -121,7 +121,7 @@ extension LHMenu: UITableViewDataSource, UITableViewDelegate {
         cell.textLabel?.textAlignment = .center
         
         cell.textLabel?.text = action.title
-        cell.textLabel?.textColor = action.isEnabled ? tintColor : #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+        cell.textLabel?.textColor = action.isEnabled ? tintColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
         return cell
     }
